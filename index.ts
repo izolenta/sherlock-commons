a5c6ef14d529e01df231f9c2087702bee84c5102
Alexey Sharov <alexey@sharov.io>
2021-10-25 09:48:52 +0200

    commons as module

---
+++ index.ts
@@ -0,0 +1,15 @@
+export {ClueItem} from "./src/model/clues/clueItem";
+export {GenericClue} from "./src/model/clues/genericClue";
+export {BoardState} from "./src/model/boardState";
+export {OneShouldBeBeforeOtherClue} from './src/model/clues/oneShouldBeBeforeOtherClue'
+export {ThreeAdjacentClue} from './src/model/clues/threeAdjacentClue'
+export {TwoAdjacentClue} from './src/model/clues/twoAdjacentClue'
+export {TwoInSameColumnClue} from './src/model/clues/twoInSameColumnClue'
+export {TwoNotInSameColumnClue} from './src/model/clues/twoNotInSameColumnClue'
+export {TwoNotAdjacentClue} from './src/model/clues/twoNotAdjacentClue'
+export {CellState} from './src/model/cellState'
+export {GameState} from './src/model/gameState'
+export {DIFFICULTY_EASY, DIFFICULTY_MEDIUM, DIFFICULTY_HARD} from './src/model/gameDifficulties'
+export * from './src/service/gameService'
+export {partyStages} from './src/model/party.stage'
+export {replaceElement, removeElement} from './src/util/util'
